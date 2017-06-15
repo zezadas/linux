62021c7750a7991efb127897f2c234729d941e51
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2017-06-14 20:42:41 -0400

    android: dc: tegra: video: conditionally revert timestamp support
    Android R14 userspace seems to have issues with timestamp support:
    
    [   14.764337] host1x host1x: SurfaceFlinger: syncpoint id 8 (disp0_a) stuck waiting 3, timeout=2147483447
    [   14.773845] host1x host1x: id 8 (disp0_a) min 2 max 4
    [   14.779163] host1x host1x: id 9 (disp1_a) min 2 max 4
    [   14.784316] host1x host1x: id 18 (2d_0) min 21 max 25
    [   14.789586] host1x host1x: id 20 (disp0_b) min 2 max 4
    [   14.794889] host1x host1x: id 21 (disp1_b) min 2 max 4
    [   14.800241] host1x host1x: id 22 (3d) min 77 max 95
    [   14.805217] host1x host1x: id 24 (disp0_c) min 2 max 4
    [   14.810573] host1x host1x: id 25 (disp1_c) min 2 max 4
    [   14.815873] host1x host1x: id 26 (vblank0) min 800 max 0
    [   14.821264] host1x host1x: id 27 (vblank1) min 684 max 0
    [   14.826810] host1x host1x: waitbase id 3 val 77
    
    Therefore conditionally revert it in that case for now.
    
    See e4e2e776a3d4bf1adf37fc061cfdfb92281f3ace.

--- drivers/staging/tegra/video/dc/dc_priv.h
+++ drivers/staging/tegra/video/dc/dc_priv.h
@@ -108,14 +108,18 @@ struct tegra_dc {
 	void				*out_data;
 
 	struct tegra_dc_mode		mode;
+#ifndef CONFIG_ANDROID
 	s64				frametime_ns;
+#endif /* !CONFIG_ANDROID */
 
 	struct tegra_dc_win		windows[DC_N_WINDOWS];
 	struct tegra_dc_blend		blend;
 	int				n_windows;
 
 	wait_queue_head_t		wq;
+#ifndef CONFIG_ANDROID
 	wait_queue_head_t		timestamp_wq;
+#endif /* !CONFIG_ANDROID */
 
 	struct mutex			lock;
 	struct mutex			one_shot_lock;
@@ -161,8 +165,10 @@ struct tegra_dc {
 	struct tegra_dc_lut		fb_lut;
 	u32				one_shot_delay_ms;
 	struct delayed_work		one_shot_work;
+#ifndef CONFIG_ANDROID
 	s64				frame_end_timestamp;
-	
+#endif /* !CONFIG_ANDROID */
+
 	struct delayed_work		disable_work;
 };
 
