f3bae1e914378654517bf35f90c4958d0358cb32
ryang <decatf@gmail.com>
2017-10-31 12:06:48 -0400

    HACK: disable nonboot cpus on kernel_restart
    Related to p4wifi reboot/shutdown hang.

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -242,6 +242,7 @@ void kernel_restart(char *cmd)
 {
 	kernel_restart_prepare(cmd);
 	migrate_to_reboot_cpu();
+	disable_nonboot_cpus();
 	syscore_shutdown();
 	if (!cmd)
 		pr_emerg("Restarting system\n");
