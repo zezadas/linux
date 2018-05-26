e780f0123f627ed9154d98d68e84d125a37e00e0
ryang <decatf@gmail.com>
2018-05-26 13:13:27 -0400

    Add p4wifi board and dts

--- arch/arm/mach-tegra/tegra.c
+++ arch/arm/mach-tegra/tegra.c
@@ -101,6 +101,10 @@ static void __init tegra_dt_init_late(void)
 	if (IS_ENABLED(CONFIG_ARCH_TEGRA_2x_SOC) &&
 	    of_machine_is_compatible("nvidia,tegra20"))
 		platform_device_register_simple("tegra20-cpufreq", -1, NULL, 0);
+
+	if (IS_ENABLED(CONFIG_ARCH_TEGRA_2x_SOC) &&
+	    of_machine_is_compatible("samsung,p4wifi"))
+		p4wifi_machine_init();
 }
 
 static const char * const tegra_dt_board_compat[] = {
