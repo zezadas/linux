13404b353fecf5fa9a276eb020181b4e7802cac4
Dmitry Osipenko <digetx@gmail.com>
2018-12-05 16:31:12 +0300

    WIP: drm/tegra: Introduce new Host1x driver and new v2 UAPI
    The new driver and UAPI provide all necessary features that are
    necessary for implementing of a proper userspace driver. The new driver
    takes a different approach in regards to working with sync points by
    making each submitted job to take an individual sync point, this solves
    the problem with the sync point recovery that old driver suffered from.
    The new Host1x driver and v2 UAPI are optimized for performance and
    minimal resources consumption. The new v2 UAPI exposes to userspace the HW
    cmdstream-related features that are necessary for a proper HW fencing.
    The staging v1 UAPI is kept functional and all of current userspace will
    continue to work with the new driver, note that some of the
    never-really-used features of v1 UAPI have been removed (like sync point
    increment IOCTL) and will return EPERM.
    
    Key moments of v2 UAPI:
    	- Raw sync points are not exposed to userspace.
    
    	- Job descriptors are embedded into the commands stream. Kernel
    	  driver parses the stream and patches the descriptors in-place.
    
    	- Commands buffer is copied from userspace via usrptr (no BO
    	  allocation and uncached-reading overhead).
    
    	- Channel is not restricted to a single client, thus 3d channel can
    	  take a multi-client job that uses 2d / 3d.
    
    	- Supports explicit jobs fencing. DRM sync object and DRM scheduler
    	  are utilized to provide the fencing support.
    
    	- Allows to use host1x gather opcode, which is restricted to
    	  data-upload usecase only. This allows userspace to pre-allocate
    	  a gather buffer, put shaders code / data there, and use two-word
    	  "gather" opcode to upload data from the buffer without pushing
    	  that data into the commands stream on each submission.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/gpu/drm/tegra/gem.h
+++ drivers/gpu/drm/tegra/gem.h
@@ -17,7 +17,10 @@
 #include <drm/drmP.h>
 #include <drm/drm_gem.h>
 
-#define TEGRA_BO_BOTTOM_UP (1 << 0)
+#include <linux/reservation.h>
+
+#define TEGRA_BO_BOTTOM_UP		(1 << 0)
+#define TEGRA_BO_HOST1X_GATHER		(1 << 1)
 
 enum tegra_bo_tiling_mode {
 	TEGRA_BO_TILING_MODE_PITCH,
@@ -32,18 +35,30 @@ struct tegra_bo_tiling {
 
 struct tegra_bo {
 	struct drm_gem_object gem;
+	struct host1x_bo *host1x_bo;
+	unsigned long dma_attrs;
 	unsigned long flags;
 	struct sg_table *sgt;
+	dma_addr_t gartaddr;
+	dma_addr_t dmaaddr;
 	dma_addr_t paddr;
+	void *dma_cookie;
 	void *vaddr;
 
-	struct drm_mm_node *mm;
+	struct drm_mm_node mm;
+	struct list_head mm_eviction_entry;
 	unsigned long num_pages;
 	struct page **pages;
 	/* size of IOMMU mapping */
-	size_t size;
+	size_t iosize;
+	/* IOMMU mapping reference counting */
+	unsigned int iomap_cnt;
 
 	struct tegra_bo_tiling tiling;
+
+	/* normally (resv == &_resv) except for imported bo's */
+	struct reservation_object *resv;
+	struct reservation_object _resv;
 };
 
 static inline struct tegra_bo *to_tegra_bo(struct drm_gem_object *gem)
@@ -52,11 +67,11 @@ static inline struct tegra_bo *to_tegra_bo(struct drm_gem_object *gem)
 }
 
 struct tegra_bo *tegra_bo_create(struct drm_device *drm, size_t size,
-				 unsigned long flags);
+				 unsigned long drm_flags, bool want_kmap);
 struct tegra_bo *tegra_bo_create_with_handle(struct drm_file *file,
 					     struct drm_device *drm,
 					     size_t size,
-					     unsigned long flags,
+					     unsigned long drm_flags,
 					     u32 *handle);
 void tegra_bo_free_object(struct drm_gem_object *gem);
 int tegra_bo_dumb_create(struct drm_file *file, struct drm_device *drm,
@@ -72,5 +87,7 @@ struct dma_buf *tegra_gem_prime_export(struct drm_device *drm,
 				       int flags);
 struct drm_gem_object *tegra_gem_prime_import(struct drm_device *drm,
 					      struct dma_buf *buf);
+
+void *tegra_bo_vmap(struct tegra_bo *bo);
 
 #endif
